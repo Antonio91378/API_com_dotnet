fbfe3b3c08aa1433bb42cd0f033fd092075ecd65
Antonio Dias <antoniovictor200@gmail.com>
2022-07-13 14:31:41 -0300

    <FEAT>:<Implementando validação em campos via atributo>

--- MyFirstAPIWithAlura/Models/Filme.cs
+++ MyFirstAPIWithAlura/Models/Filme.cs
@@ -1,10 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace MyFirstAPIWithAlura.Controllers
 {
     public class Filme
     {
+        [Required(ErrorMessage = "O campo titulo é obrigatorio")]
         public string Titulo { get; set; }
+        [Required]
         public string Diretor { get; set; }
         public string Genero { get; set; }
+        [Range(1, 600)]
         public int Duracao { get; set; }
     }
 }
